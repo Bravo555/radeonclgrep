0fe857b4ea082e999cde196158005c2c82f26027
Marcel Guzik <mguzik555@gmail.com>
2020-08-31 02:55:38 +0200

    thinned out main function

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,32 @@ use reqwest::{header, Client};
 use scraper::{ElementRef, Html, Selector};
 use std::env;
 use std::error::Error;
+use std::fmt::Display;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let pattern = Regex::new(&env::args().nth(1).expect("expected pattern")).unwrap();
+    let pattern = Regex::new(&format!(
+        "(?i:{})",
+        &env::args().nth(1).expect("expected pattern"),
+    ))?;
 
-    let base_url = "https://www.amd.com/en/support/kb/release-notes/rn-rad-win-";
-    let current_date = Utc::now().date().naive_utc();
-    let version_url = format!(
-        "{}{}-{}-{}",
-        base_url,
-        current_date.format("%y"),
-        current_date.month(),
-        2
-    );
+    let current_date = Utc::today();
+    let version_url = format_url(current_date.format("%y"), current_date.month(), 2);
 
+    let client = http_client()?;
+    let response = client.get(&version_url).send().await?.text().await?;
+
+    print_page_hits(&response, &pattern);
+
+    Ok(())
+}
+
+fn format_url(year: impl Display, month: impl Display, revision: u32) -> String {
+    let base_url = "https://www.amd.com/en/support/kb/release-notes/rn-rad-win";
+    format!("{}-{}-{}-{}", base_url, year, month, revision)
+}
+
+fn http_client() -> reqwest::Result<Client> {
     let mut headers = header::HeaderMap::new();
     headers.insert(
         header::ACCEPT_ENCODING,
@@ -28,9 +39,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
         header::ACCEPT_LANGUAGE,
         header::HeaderValue::from_static("en-GB,en;q=0.5"),
     );
-    let client = Client::builder().default_headers(headers).build()?;
-    let response = client.get(&version_url).send().await?.text().await?;
+    Client::builder().default_headers(headers).build()
+}
 
+fn print_page_hits(response: &str, pattern: &Regex) {
     let doc = Html::parse_document(&response);
     let sel = Selector::parse(".field--name-body > ul").unwrap();
     let lists = doc.select(&sel).take(4);
@@ -45,5 +57,4 @@ async fn main() -> Result<(), Box<dyn Error>> {
     for hit in hits {
         println!("{}", hit);
     }
-    Ok(())
 }
